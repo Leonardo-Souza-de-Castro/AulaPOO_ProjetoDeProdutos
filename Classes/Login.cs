526ff77330bb7d56fbcddf599b899e99d546ef04
Leonardo Souza <leonardo030605@gmail.com>
2021-06-08 11:11:34 -0300

    Saindo do loop do login

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -27,34 +27,16 @@ namespace AulaPOO_ProjetoDeProdutos.Classes
             {
                 Console.Write("Insira seu Email: ");
                 string email = Console.ReadLine();
-                var resposta = usuario.usuarios.FindAll(item => item.Email == email).ToString();
-                Console.WriteLine(resposta);
                 Console.Write("Insira sua Senha: ");
                 string senha = Console.ReadLine();
-                string resposta_1 = usuario.usuarios.FindAll(item => item.Senha == senha).ToString();
-                    if (resposta == email && resposta_1 == senha)
+                Usuario usuario_encontrado = usuario.usuarios.Find(item => item.Senha == senha && item.Email == email);
+                Console.WriteLine(email, senha, usuario_encontrado);
+                    if (usuario_encontrado != null)
                     {
-                        Console.WriteLine("Usuario logado com sucesso!!");
-                        Logado = true;
-                        tentar_novamente = false;
-                    }
-                    else if (resposta != email && resposta_1 == senha)
+                        Console.WriteLine("Usuario encontrado!");
+                    }else
                     {
-                        Console.WriteLine("Email errado, por favor tente novamente");
-                        Logado = false;
-                        tentar_novamente = true;
-                    }
-                    else if (resposta == email && resposta_1 != senha)
-                    {
-                        Console.WriteLine("Senha errada, por favor tente novamente");
-                        Logado = false;
-                        tentar_novamente = true;
-                    }
-                    else if (resposta != email && resposta_1 != senha)
-                    {
-                        Console.WriteLine("Senha e Email errados por favor verifique se você ja se cadastrou");
-                        Logado = false;
-                        tentar_novamente = true;
+                        Console.WriteLine("Usuario ou senha incorretos");
                     }
                
             } while (tentar_novamente);
